3baabc010dd520a12ee84fa7df0aa18ae9a04bec
AlekseyTsvetaev <hellsong649@gmail.com>
2025-01-26 11:22:16 +0300

    last commit

--- src/constants/cypres-constants.js
+++ src/constants/cypres-constants.js
@@ -5,3 +5,7 @@ export const modalOverlay = '[data-testid="modal-overlay"]';
 export const orderDetailsModal = '[data-testid="order-details-modal"]';
 export const constructorBurger = '[data-testid="constructor-burger"]';
 export const modalCloseIcon = '[data-testid="modal-close-icon"]';
+export const bunTop = '[data-testid="bun-top"]';
+export const bunBottom = '[data-testid="bun-bottom"]';
+
+export const ingredientRoute = '/643d69a5c3f7b9001cfa093c';
